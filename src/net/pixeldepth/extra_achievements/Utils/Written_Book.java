71f554072d2519893ffee8c030e1b12bf064c1cf
Peter <github@pixeldepth.net>
2015-07-11 12:54:55 +0100

    Added comment about the book and what it will be for

--- src/net/pixeldepth/extra_achievements/Utils/Written_Book.java
+++ src/net/pixeldepth/extra_achievements/Utils/Written_Book.java
@@ -6,6 +6,11 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 import org.json.simple.JSONObject;
 
+/**
+ * This class will handle creating a written book for the player so that they
+ * can view all their achievements, including vanilla (note to self: add config option for that).
+ */
+
 public final class Written_Book {
 
 	public Written_Book(Player player, String uuid, JSONObject data){
@@ -14,6 +19,9 @@ public final class Written_Book {
 
 		meta.setTitle("Achievements");
 		meta.setAuthor("The Achievements Collector"); // Change to something better later
+
+		// Test
+
 		meta.addPage("apples", "pears");
 
 		book.setItemMeta(meta);
